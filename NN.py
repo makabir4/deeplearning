f7fdc923c6b156ec5d4cf383e602506efd6908f6
Md Alamgir Kabir <makabir4-c@my.cityu.edu.hk>
2023-04-27 14:01:43 +0200

    Update NN.py

--- NN.py
+++ NN.py
@@ -6,3 +6,21 @@ Created on Wed Mar  1 14:39:53 2023
 @author: mkr01
 """
 
+from sklearn.datasets import load_iris
+from pymfe.mfe import MFE
+
+data = load_iris()
+
+y = data.target
+x = data.data
+
+print(x)
+print(y)
+
+mfe = MFE(groups=["all"])
+
+mfe.fit(x, y)
+
+ft = mfe.extract()
+
+print(ft)
